ecacf674728f0ed76517bc91add6aaf4ba3a9d9a
MichalJakubczak <mich.jak2000@gmail.com>
2024-01-06 20:45:53 +0100

    errors fix

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -2,9 +2,9 @@ import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next)=>{
     console.log(err);
-    const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
-    const message = err.message || 'coś poszło nie tak, spróbuj ponownie później...'
-    res.status(statusCode).json({message:'Coś poszło nie tak'});
+    const statusCode = err.statusCode || StatusCodes.BAD_REQUEST;
+    const message = err.message || 'Coś poszło nie tak';
+    res.status(statusCode).json({message});
 };
 
 export default errorHandlerMiddleware;
