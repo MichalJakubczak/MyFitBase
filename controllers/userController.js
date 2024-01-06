c67e3bab1ea95682df4b72b2e9b15d80533f46e9
MichalJakubczak <mich.jak2000@gmail.com>
2024-01-06 13:11:35 +0100

    delete user(to fix) and password hide req

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,9 @@ import User from "../models/UserModel.js";
 import Excersise from "../models/ExcersiseModel.js";
 
 export const getCurrentUser = async (req,res) =>{
-    res.status(StatusCodes.OK).json({message:'aktualny użytkownik'});
+    const user = await User.findOne({_id:req.user.userId});
+    const userWithoutPassword = user.toJSON();
+    res.status(StatusCodes.OK).json({user: userWithoutPassword});
 };
 
 export const getApplicationStats = async (req,res) =>{
@@ -12,4 +14,9 @@ export const getApplicationStats = async (req,res) =>{
 
 export const editUser = async (req,res) =>{
     res.status(StatusCodes.OK).json({message:'Edycja użytkownika'});
+};
+
+export const deleteUser = async (req,res) =>{
+    const removedUser = await User.findByIdAndDelete(req.params.id)
+    res.status(StatusCodes.OK).json({message:'Użytkownik usunięty!'});
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { editUser, getApplicationStats, getCurrentUser } from "../controllers/userController.js";
+import { editUser, getApplicationStats, getCurrentUser, deleteUser } from "../controllers/userController.js";
 
 const router = Router();
 
 router.get('/current-user', getCurrentUser );
 router.get('/admin/app-stats', getApplicationStats);
 router.patch('/edit-user', editUser);
+router.delete('/delete-user', deleteUser);
 
 
 export default router;
