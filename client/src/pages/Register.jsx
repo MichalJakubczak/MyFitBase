c9d70c515df5daba81e64182c73a6b31161d3c4b
MichalJakubczak <mich.jak2000@gmail.com>
2024-01-06 20:17:44 +0100

    errors in register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { Link, Form, redirect, useNavigation } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from "../utils/customFetch";
+import {toast} from 'react-toastify';
 
 /*
 export const action = async ({ request }) =>{
@@ -20,10 +21,11 @@ export const action = async ({request}) =>{
   
   try {
     await customFetch.post('/auth/register',data)
+    toast.success('Rejestracja zakończona sukcesem')
     return redirect('/login');
 
   } catch (error) {
-    console.log(error);
+    toast.error(error?.response?.data?.message);
     return error;
   }
 
@@ -31,6 +33,9 @@ export const action = async ({request}) =>{
 };
 
 const Register = () => {
+  const navigation = useNavigation()
+  console.log(navigation);
+  const isSubmitting = navigation.state === 'submitting'
   return (
   <Wrapper>
     <Form method='post' className="form">
@@ -39,7 +44,7 @@ const Register = () => {
       <FormRow type='text' name='username' labelText='Nazwa użytkownika' defaultValue='Deadlyver'/>
       <FormRow type='email' name='email' labelText='email' defaultValue='fitbase@gmail.com'/>
       <FormRow type='password' name='password' labelText='hasło' defaultValue='secret123'/>
-      <button type="submit" className="btn btn-block">Utwórz konto</button>
+      <button type="submit" className="btn btn-block" disabled ={isSubmitting} >{isSubmitting ? 'Rejestracja...':'Zarejestruj'}</button>
       
       <p>
         Masz już konto? Kliknij tutaj aby się zalogować!<br></br>
