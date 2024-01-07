dcd261ff4933d16d904e500bf7c72d362bf6ec71
MichalJakubczak <mich.jak2000@gmail.com>
2024-01-07 17:53:40 +0100

    exercise type select row

--- client/src/pages/AddExercise.jsx
+++ client/src/pages/AddExercise.jsx
@@ -1,4 +1,4 @@
-import { FormRow } from '../components';
+import { FormRow, FormRowSelect } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { useOutletContext } from 'react-router-dom';
 import { EXERCISE_STATUS } from '../../../utils/constants';
@@ -19,6 +19,9 @@ const AddExercise = () => {
         <FormRow type='text' name='mainMuscle' labelText='Główna partia mięśniowa'/>
         <FormRow type='text' name='addsMuscle' labelText='Dodatkowa partia mięśniowa'/>
         <FormRow type='text' name='excersiseDescription' labelText='Opis ćwiczenia'/>
+        <FormRowSelect labelText='Typ ćwiczenia' 
+        name='excersiseType' 
+        defaultValue={EXERCISE_STATUS.ALL} list={Object.values(EXERCISE_STATUS)}/>
         <button type='submit' className='btn btn-block form-btn' disabled={isSubmitting}>{isSubmitting?'Dodawanie...':'Dodaj'}</button>
       </div>
     </Form>
